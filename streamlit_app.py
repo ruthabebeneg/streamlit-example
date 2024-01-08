5754730c74edd92fd41ad6679ac2e90146f211cb
ruthabebeneg <124071594+ruthabebeneg@users.noreply.github.com>
2024-01-08 14:47:35 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,81 +10,81 @@ import os
 
 os.environ["TF_CPP_MIN_LOG_LEVEL"] = "2"
 
-gpu_devices = tf.config.experimental.list_physical_devices('GPU')
-if any(gpu_devices):
-    tf.config.experimental.set_memory_growth(gpu_devices[0], True)
+def main():
+    gpu_devices = tf.config.experimental.list_physical_devices('GPU')
+    if any(gpu_devices):
+        tf.config.experimental.set_memory_growth(gpu_devices[0], True)
 
-# Mode large
-st.set_page_config(layout="wide")
+    # Mode large
+    st.set_page_config(layout="wide")
 
-# Conception de l'interface
-st.title("docTR : Reconnaissance de texte dans les documents")
-# Pour une nouvelle ligne
-st.write('\n')
-# Instructions
-st.markdown("*Astuce : cliquez sur le coin supérieur droit d'une image pour l'agrandir !*")
-# Définir les colonnes
+    # Conception de l'interface
+    st.title("docTR : Reconnaissance de texte dans les documents")
+    # Pour une nouvelle ligne
+    st.write('\n')
+    # Instructions
+    st.markdown("*Astuce : cliquez sur le coin supérieur droit d'une image pour l'agrandir !*")
+    # Définir les colonnes
 
+    # Sélection de fichiers
+    uploaded_files = st.file_uploader("Télécharger des fichiers", type=['pdf', 'png', 'jpeg', 'jpg'], accept_multiple_files=True)
 
-# Sélection de fichiers
-uploaded_files = st.file_uploader("Télécharger des fichiers", type=['pdf', 'png', 'jpeg', 'jpg'], accept_multiple_files=True)
+    if uploaded_files is not None:
+        for uploaded_file in uploaded_files:
+            # Votre logique de traitement pour chaque fichier ici
+            if uploaded_file.name.endswith('.pdf'):
+                doc = DocumentFile.from_pdf(uploaded_file.read())
+            else:
+                doc = DocumentFile.from_images(uploaded_file.read())
 
+            for page_idx in range(len(doc)):
+                st.write(f"**Page {page_idx + 1}/{len(doc)}**")
 
-# ...
+                # Sélection du modèle (automatique) - déplacé à l'intérieur de la boucle
+                det_arch = "db_resnet50" if doc[page_idx].shape[0] > 1000 else "linknet_resnet18_rotation"
+                reco_arch = "crnn_mobilenet_v3_small"
 
-if uploaded_files is not None:
-    for uploaded_file in uploaded_files:
-        # Votre logique de traitement pour chaque fichier ici
-        if uploaded_file.name.endswith('.pdf'):
-            doc = DocumentFile.from_pdf(uploaded_file.read())
-        else:
-            doc = DocumentFile.from_images(uploaded_file.read())
+                with st.spinner('Chargement du modèle...'):
+                    predictor = ocr_predictor(
+                        det_arch, reco_arch, pretrained=True,
+                        assume_straight_pages=(det_arch != "linknet_resnet18_rotation")
+                    )
 
-        for page_idx in range(len(doc)):
-            st.write(f"**Page {page_idx + 1}/{len(doc)}**")
+                with st.spinner('Analyse en cours...'):
+                    # Transmettre l'image au modèle
+                    processed_batches = predictor.det_predictor.pre_processor([doc[page_idx]])
+                    out = predictor.det_predictor.model(processed_batches[0], return_model_output=True)
+                    seg_map = out["out_map"]
+                    seg_map = tf.squeeze(seg_map[0, ...], axis=[2])
+                    seg_map = cv2.resize(seg_map.numpy(), (doc[page_idx].shape[1], doc[page_idx].shape[0]),
+                                         interpolation=cv2.INTER_LINEAR)
+                    # Afficher la carte thermique brute
+                    fig, ax = plt.subplots()
+                    ax.imshow(seg_map)
+                    ax.axis('off')
 
-            # Sélection du modèle (automatique) - déplacé à l'intérieur de la boucle
-            det_arch = "db_resnet50" if doc[page_idx].shape[0] > 1000 else "linknet_resnet18_rotation"
-            reco_arch = "crnn_mobilenet_v3_small"
+                    # Ajouter une barre de progression pour l'analyse OCR
+                    progress_bar = st.progress(0)
+                    for i in range(100):
+                        # Simuler un processus OCR chronophage
+                        time.sleep(0.1)
+                        progress_bar.progress(i + 1)
 
-            with st.spinner('Chargement du modèle...'):
-                predictor = ocr_predictor(
-                    det_arch, reco_arch, pretrained=True,
-                    assume_straight_pages=(det_arch != "linknet_resnet18_rotation")
-                )
+                    # Afficher la sortie OCR
+                    out = predictor([doc[page_idx]])
+                    fig = visualize_page(out.pages[0].export(), doc[page_idx], interactive=False)
 
-            with st.spinner('Analyse en cours...'):
-                # Transmettre l'image au modèle
-                processed_batches = predictor.det_predictor.pre_processor([doc[page_idx]])
-                out = predictor.det_predictor.model(processed_batches[0], return_model_output=True)
-                seg_map = out["out_map"]
-                seg_map = tf.squeeze(seg_map[0, ...], axis=[2])
-                seg_map = cv2.resize(seg_map.numpy(), (doc[page_idx].shape[1], doc[page_idx].shape[0]),
-                                     interpolation=cv2.INTER_LINEAR)
-                # Afficher la carte thermique brute
-                fig, ax = plt.subplots()
-                ax.imshow(seg_map)
-                ax.axis('off')
+                    # Reconstitution de la page sous la page d'entrée
+                    if det_arch != "linknet_resnet18_rotation":
+                        img = out.pages[0].synthesize()
 
-                # Ajouter une barre de progression pour l'analyse OCR
-                progress_bar = st.progress(0)
-                for i in range(100):
-                    # Simuler un processus OCR chronophage
-                    time.sleep(0.1)
-                    progress_bar.progress(i + 1)
+                    # Afficher le texte extrait
+                    text_content = ""
+                    for block in out.pages[0].blocks:
+                        for line in block.lines:
+                            for word in line.words:
+                                text_content += word.value + " "
+                    st.write("**Texte extrait :**", text_content)
 
-                # Afficher la sortie OCR
-                out = predictor([doc[page_idx]])
-                fig = visualize_page(out.pages[0].export(), doc[page_idx], interactive=False)
-
-                # Reconstitution de la page sous la page d'entrée
-                if det_arch != "linknet_resnet18_rotation":
-                    img = out.pages[0].synthesize()
-
-                # Afficher le texte extrait
-                text_content = ""
-                for block in out.pages[0].blocks:
-                    for line in block.lines:
-                        for word in line.words:
-                            text_content += word.value + " "
-                st.write("**Texte extrait :**", text_content)
+if __name__ == "__main__":
+    main()
